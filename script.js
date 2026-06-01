f29d13d23d203748a32e305cb527ed7509754b6b
Shreya Palase <59469602+shreyapalase@users.noreply.github.com>
2026-06-02 02:18:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-script
+function openGame(name) {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("gameContainer").classList.remove("hidden");
+
+  document.getElementById("gameFrame").src = "games/" + name + ".html";
+}
+
+function exitGame() {
+  document.getElementById("gameContainer").classList.add("hidden");
+  document.getElementById("home").style.display = "flex";
+  document.getElementById("gameFrame").src = "";
+}
