0066633263c730e024246a61a85b0a25cbe01b45
Shreya Palase <59469602+shreyapalase@users.noreply.github.com>
2026-06-02 13:44:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -112,3 +112,54 @@ function exitGame() {
     .getElementById("gameFrame")
     .src = "";
 }
+/* MOUSE ENERGY FOLLOWER */
+
+const glow =
+document.getElementById("mouseGlow");
+
+document.addEventListener("mousemove", e => {
+
+glow.style.left =
+e.clientX + "px";
+
+glow.style.top =
+e.clientY + "px";
+});
+
+
+/* 3D CARD TILT */
+
+document.querySelectorAll(".card")
+.forEach(card=>{
+
+card.addEventListener("mousemove",e=>{
+
+const rect=
+card.getBoundingClientRect();
+
+const x=
+e.clientX-rect.left;
+
+const y=
+e.clientY-rect.top;
+
+const rotateY=
+((x/rect.width)-0.5)*20;
+
+const rotateX=
+((y/rect.height)-0.5)*-20;
+
+card.style.transform=
+`perspective(1000px)
+rotateX(${rotateX}deg)
+rotateY(${rotateY}deg)
+scale(1.08)`;
+});
+
+card.addEventListener("mouseleave",()=>{
+
+card.style.transform=
+"perspective(1000px) rotateX(0deg) rotateY(0deg)";
+});
+
+});
